fa419a356ff61f3d558dea7f4b785642c21d106e
ALAN SAMUEL VAZQUEZ RIVERA <avr2654@eglobal.com.mx>
2024-08-03 10:16:10 -0600

    Se agrega archivo de localizadores para PRP Cocilia

---
+++ POM/data/prpConcilia/prpConciliaLocalizadores.ts
@@ -0,0 +1,9 @@
+export const LOCALIZADOR = {
+    //MENUS
+    REP_ESPECIAL: 'a.smoothHeader:has-text("Reportes Especiales")',
+    DIF_TASAS: 'a.smoothHeader:has-text("Dif Tasas")',
+    BANCO_AZTECA: 'a.smoothHeader:has-text("Banco Azteca")',
+    //SUBMENUS
+    
+}
+
